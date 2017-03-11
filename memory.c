a68b47397989afa1e649a42d80b7bd2467aa73b5
kartik <kartik9310@gmail.com>
2017-03-11 19:48:47 +0800

    Commit Messsage : Hello, Done

---
+++ memory.c
@@ -0,0 +1,11 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/slab.h>
+#include <linux/errno.h>
+#include <linux/types.h>
+#include <linux/fs.h>
+#include <linux/proc_fs.h>
+#include <asm/uaccess.h>
+
+#define MAJOR_NUMBER 61
